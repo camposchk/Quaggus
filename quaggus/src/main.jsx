619f76fe445f3f219e8e3ebcebceb71842f3d26b
Juan Campos <juancampos.chk@gmail.com>
2024-04-23 08:48:11 -0300

    Analytics

--- quaggus/src/main.jsx
+++ quaggus/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Login from './login.jsx'
+import { Analytics } from "@vercel/analytics/react"
 import './index.css'
 
 import {
@@ -22,6 +23,7 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Analytics />
     <RouterProvider router={router} />
   </React.StrictMode>
 )
