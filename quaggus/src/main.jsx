d2bf1cea020f80dbacd7427fdd5cd2f5ce8db340
Juan Campos <juancampos.chk@gmail.com>
2024-04-23 08:50:18 -0300

    SpeedInsights

--- quaggus/src/main.jsx
+++ quaggus/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Login from './login.jsx'
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import './index.css'
 
 import {
@@ -24,6 +25,7 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Analytics />
+    <SpeedInsights />
     <RouterProvider router={router} />
   </React.StrictMode>
 )
