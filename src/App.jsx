3bee237dd17f657b0196ef4c16beb10db95ab1fd
Juan Campos <juancampos.chk@gmail.com>
2024-04-23 09:22:39 -0300

    Folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import style from './App.module.css'
 import { Navbar } from './components/Navbar'
 import { MainCard } from './components/MainCard'
+import { Cards } from './components/Cards'
 
 function App() {
 
   return (
     <main className={style["background"]}>
       <MainCard />
+      <Cards />
       <Navbar />
     </main>
   )
---
+++ src/components/Cards.jsx
@@ -0,0 +1,28 @@
+import style from "./Cards.module.css"
+import camiseta2 from "../assets/Images/camiseta2.png"
+import camiseta3 from "../assets/Images/camiseta3.png"
+import camiseta4 from "../assets/Images/camiseta4.png"
+
+export const Cards = () => {
+    return(
+        <>
+            <div className={style["center"]}>
+                <card className={style["card-styling"]}>
+                    <div className={style["center"]}>
+                        <img src={camiseta2} alt="Camiseta Real Madrid" style={{marginTop: 51}}/>
+                    </div>
+                </card>
+                <card className={style["card-styling"]}>
+                    <div className={style["center"]}>
+                        <img src={camiseta3} alt="Camiseta Real Madrid" style={{marginTop: 51}}/>
+                    </div>
+                </card>
+                <card className={style["card-styling"]}>
+                    <div className={style["center"]}>
+                        <img src={camiseta4} alt="Camiseta Real Madrid" style={{marginTop: 51}}/>
+                    </div>
+                </card>
+            </div>
+        </>
+    )
+}
